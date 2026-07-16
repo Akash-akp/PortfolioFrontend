09b4aabeb67fb10f1c0464ccf4a05ecde5cde42d
Akash-akp <akashakp0037@gmail.com>
2026-07-17 02:43:32 +0530

    changed backend uri

--- src/lib/backend-endpoint.ts
+++ src/lib/backend-endpoint.ts
@@ -1,3 +1,3 @@
-// export const backendUri = "https://portfoliobackend-xv0y.onrender.com";
-export const backendUri = "http://localhost:8080";
+export const backendUri = "https://portfoliobackend-xv0y.onrender.com";
+// export const backendUri = "http://localhost:8080";
 export const portfolioId = 4;
